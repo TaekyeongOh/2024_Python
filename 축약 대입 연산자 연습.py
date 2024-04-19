0fc2e75efef74035933be361ca0fa7b748ef34d9
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-04-19 13:14:58 +0900

    중간고사 연습

---
+++ 축약 대입 연산자 연습.py
@@ -0,0 +1,8 @@
+def show_msg():
+    print('hello')
+    return 'good bye'
+
+print('시작')
+result = show_msg()
+print(result)
+print('마침')
---
+++ 축약 대입 연산자 연습_매개변수 이용.py
@@ -0,0 +1,9 @@
+def show_msg():
+    print('hello')
+    name=str(input('이름?'))
+    return name
+
+print('시작')
+result=show_msg()
+print(result)
+print('마침')
